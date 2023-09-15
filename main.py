503919fee03664759dbf2cc235adc90ddea417f6
hamzaashfaque <hamzaashfaque000@gmail.com>
2023-09-15 18:54:40 +0500

    ver2

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from scraper_functions import get_card_links
+from scraper_functions import get_card_links, get_anime_info
 import pandas as pd
 
-LINK = "https://myanimelist.net/topanime.php?limit="
-card_links = get_card_links(LINK)
+# LINK = "https://myanimelist.net/topanime.php?limit="
+# card_links = get_card_links(LINK)
+# df = pd.DataFrame(card_links, columns=['card_links'])
+# df.to_csv("data/card_links.csv")
 
-df = pd.DataFrame(card_links, columns=['card_links'])
-df.to_csv("data/card_links.csv")
+test_links = ["https://myanimelist.net/anime/2921/Ashita_no_Joe_2",
+              "https://myanimelist.net/anime/457/Mushishi",
+              "https://myanimelist.net/anime/33352/Violet_Evergarden",
+              "https://myanimelist.net/anime/33050/Fate_stay_night_Movie__Heavens_Feel_-_III_Spring_Song"]
+
+get_anime_info(test_links)
--- scraper_functions.py
+++ scraper_functions.py
@@ -1,14 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import tqdm
 
 
 def get_card_links(link):
-    driver = webdriver.Chrome
+    driver = webdriver.Chrome()
     card_links = []
     end_page = False
     limit = 0
     while(end_page is False):
         driver.get(f"{link}{limit}")
+        driver.implicitly_wait(5)
         print(f"Gathering anime titles {limit} to {limit+50}")
         header = driver.find_element(By.CLASS_NAME, "h1").get_attribute("innerHTML")
         if header == "404 Not Found":
@@ -19,8 +21,13 @@ def get_card_links(link):
             href = tag.get_attribute("href")
             card_links.append(href)
         limit += 50
+    print("Links Gathered Successfully!")
     return card_links
 
 
 
-#def get_anime_info(card_links):
+def get_anime_info(card_links):
+    driver = webdriver.Chrome()
+    for link in card_links:
+        driver.get(link)
+        driver.implicitly_wait(5)
